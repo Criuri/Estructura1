d9a9d04e4a5391fdf4c7c361c18f4b51dc75f902
Sala K 404 <salak404@salasfr.itm.edu.co>
2024-04-26 07:01:41 -0500

    Reconfiguracion Getters and setters

--- Empleado.java
+++ Empleado.java
@@ -16,16 +16,31 @@ public class Empleado {
     public String getNombre() {
         return Nombre;
     }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
     public String getApellido() {
         return Apellido;
+    }
+    public void setApellido(String apellido) {
+        Apellido = apellido;
     }
     public String getDireccion() {
         return Direccion;
     }
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
     public int getEdad() {
         return Edad;
     }
+    public void setEdad(int edad) {
+        Edad = edad;
+    }
     public String getCargo() {
         return Cargo;
+    }
+    public void setCargo(String cargo) {
+        Cargo = cargo;
     }
 }
